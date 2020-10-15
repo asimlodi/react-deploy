53622a57ccc36421a190f9848d944298d9d39fba
asimlodi <asimumair@outlook.com>
2020-10-15 06:55:05 -0700

    Updates

---
+++ precache-manifest.18d0d413d39d9b056dc7812504b5876c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c953cff59984e17c4ae4a395ccc395b",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "326a440ed06d4f4afebd",
+    "url": "/react-deploy/static/css/main.6a497dae.chunk.css"
+  },
+  {
+    "revision": "8792d30f9c7f720ccf85",
+    "url": "/react-deploy/static/js/2.cd6e740c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-deploy/static/js/2.cd6e740c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326a440ed06d4f4afebd",
+    "url": "/react-deploy/static/js/main.3f1c8e2d.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "eda3f3f1157747fe3a2822862996a788",
+    "url": "/react-deploy/static/media/project-2.eda3f3f1.jpg"
+  }
+]);
